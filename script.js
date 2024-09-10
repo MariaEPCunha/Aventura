6f0b811b994bf20dbc095b5242e50bd72414e641
MariaEPCunha <00001122176910sp@al.educacao.sp.gov.br>
2024-09-09 22:52:45 -0300

    Update script.js
    atualizando arquivo js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querSelector('.ativo');
-        const proximoPasso = 'passo-' + this.getTribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remover('ativo');
+        atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
